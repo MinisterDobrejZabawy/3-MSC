2d946e649674a616969924900a29df50355d41fb
MinisterDobrejZabawy <alfrex2007@gmail.com>
2023-11-02 01:57:56 +0100

    Update 1 - "And I Said, 'Let the Header Appear'"
    - Created a header featuring the website's menu, a social media menu, and a language menu (I'm sooooo proud of it ^^).
    - Designed the title section.
    - Added a cool background.

--- src/script/js/prlx.js
+++ src/script/js/prlx.js
@@ -1,26 +1,20 @@
-(function() {
-    // Add event listener
-    document.addEventListener("mousemove", parallax);
-    const images = [
-        document.querySelector("#image1"),
-        document.querySelector("#image2"),
-        document.querySelector("#image3"),
-        document.querySelector("#image4")
-    ];
+<script>
+  document.addEventListener("DOMContentLoaded", function () {
+    const introImgContainer = document.querySelector(".intro-img-container");
+    const introTitle = document.querySelector(".intro-title");
+    const body = document.querySelector("body");
 
-    // Magic happens here
-    function parallax(e) {
-        let _w = window.innerWidth/2;
-        let _h = window.innerHeight/2;
-        let _mouseX = e.clientX;
-        let _mouseY = e.clientY;
+    body.addEventListener("mousemove", (e) => {
+      const mouseX = e.clientX;
+      const mouseY = e.clientY;
+      const centerX = window.innerWidth / 2;
+      const centerY = window.innerHeight / 2;
 
-        images.forEach((image, index) => {
-            let speedX = (index - 1) * 0.0005; // Prędkość w poziomie
-            let speedY = (index - 1) * 0.01; // Prędkość w pionie
-            let _depthX = `${50 - (_mouseX - _w) * speedX}%`;
-            let _depthY = `${50 - (_mouseY - _h) * speedY}%`;
-            image.style.backgroundPosition = `${_depthX} ${_depthY}`;
-        });
-    }
-})();
+      const moveX = (mouseX - centerX) / 200;
+      const moveY = (mouseY - centerY) / 400;
+
+      introImgContainer.style.transform = `translate(${-moveX}px, ${-moveY}px)`;
+      introTitle.style.transform = `translate(${moveX}px, ${moveY}px)`;
+    });
+  });
+</script>
