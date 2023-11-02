e799f31355d3d8019f50dc53e23a06449efa89a9
MinisterDobrejZabawy <alfrex2007@gmail.com>
2023-11-02 02:11:33 +0100

    Update 3 - "And I Said, 'Let the JS Script Be Relocated'"
    - I've just moved the internal JS script from the HTML code to a separate JS file. Well, that's it :p

--- src/script/js/prlx.js
+++ src/script/js/prlx.js
@@ -1,20 +1,31 @@
-<script>
-  document.addEventListener("DOMContentLoaded", function () {
-    const introImgContainer = document.querySelector(".intro-img-container");
-    const introTitle = document.querySelector(".intro-title");
-    const body = document.querySelector("body");
+document.addEventListener('DOMContentLoaded', function () {
+  const introImgContainer = document.querySelector(
+    '.intro-img-container'
+  );
+  const introTitle = document.querySelector('.intro-title');
+  const background = document.querySelector('.background-image');
+  const body = document.querySelector('body');
+  let mouseX = 0;
+  let mouseY = 0;
 
-    body.addEventListener("mousemove", (e) => {
-      const mouseX = e.clientX;
-      const mouseY = e.clientY;
-      const centerX = window.innerWidth / 2;
-      const centerY = window.innerHeight / 2;
-
-      const moveX = (mouseX - centerX) / 200;
-      const moveY = (mouseY - centerY) / 400;
-
-      introImgContainer.style.transform = `translate(${-moveX}px, ${-moveY}px)`;
-      introTitle.style.transform = `translate(${moveX}px, ${moveY}px)`;
-    });
+  body.addEventListener('mousemove', (e) => {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
   });
-</script>
+
+  const animate = () => {
+    const centerX = window.innerWidth / 2;
+    const centerY = window.innerHeight / 2;
+
+    const moveX = (mouseX - centerX) / 400;
+    const moveY = (mouseY - centerY) / 400;
+
+    introImgContainer.style.transform = `translate(${-moveX}px, ${-moveY}px)`;
+    introTitle.style.transform = `translate(${moveX}px, ${moveY}px)`;
+    background.style.transform = `translate(${moveX}px, ${moveY}px)`;
+
+    requestAnimationFrame(animate);
+  };
+
+  animate();
+});
